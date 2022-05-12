cc036f44268129a6da25a85ada26db49b96bd5da
Isadora Barros Rodrigues <isadora.rodrigues70@gmail.com>
2022-05-12 16:55:40 -0300

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,18 @@
+list = ['star', 'rats', 'car', 'arc', 'arts', 'stars']
+
+def verify_len(word, array):
+    new_list = [word]
+    for i in array:
+        if  len(word) == len(i):
+            new_list.append(i)
+    return new_list
+
+def anagram_check(array):
+    anagrams = []
+    for i in array:
+        array.remove(i)
+        anagrams.append(verify_len(i, array))
+    return anagrams
+        
+
+print(anagram_check(list))
